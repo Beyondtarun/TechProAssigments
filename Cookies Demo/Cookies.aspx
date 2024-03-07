5afad014e918a63852637786c6997ad3709ff5e9
Beyondtarun <teg.tarun@gmail.com>
2024-03-07 18:12:08 +0530

    Cookies by C#

---
+++ Cookies Demo/Cookies.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cookies.aspx.cs" Inherits="PracticeC_.Cookies" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+     Username :
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+      
+            <asp:Button runat="server" Text="Submit" OnClick="Button_Click"></asp:Button>
+            </div>
+            </form>
+</body>
+</html>
---
+++ Cookies Demo/Cookies.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PracticeC_
+{
+    public partial class Cookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("user");
+            cookie["username"] = TextBox1.Text;
+            cookie.Expires = DateTime.Now.AddDays(2); // DateTime method makes cookie persistent 
+            Response.Cookies.Add(cookie);
+            Response.Redirect("WebForm4.aspx");
+        }
+    }
+}
