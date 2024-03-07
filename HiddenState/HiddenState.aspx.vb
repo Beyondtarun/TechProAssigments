2eab33697907917d3fa2abe5a165f0d853a657c4
Beyondtarun <teg.tarun@gmail.com>
2024-03-07 16:22:40 +0530

    Hidden State in Vb.Net

---
+++ HiddenState/HiddenState.aspx.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Reflection.Emit
+
+Public Class HiddenState
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        hiddentext.Value = DateTime.Now.ToString()
+        Label1.Text = "Hidden field Value " + hiddentext.Value
+    End Sub
+
+End Class
