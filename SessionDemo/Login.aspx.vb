52b95160aef6b63e2d91d3008839e687ed40e44f
Beyondtarun <teg.tarun@gmail.com>
2024-03-07 15:39:53 +0530

    Session Demo by VB

---
+++ SessionDemo/Login.aspx.vb
@@ -0,0 +1,20 @@
+﻿Public Class Login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub LOGIN(ByVal sender As Object, ByVal e As System.EventArgs)
+        If Textbox2.Text.ToString() = "pass" AndAlso Textbox1.Text.ToString() IsNot "" Then
+            Session("UserName") = Textbox1.Text.ToUpper()
+            Response.Redirect("WebForm1.aspx?UserName=" & Textbox1.Text.ToString())
+        Else
+            Label1.Text = "WRONG PASSWORD"
+
+
+        End If
+    End Sub
+
+
+End Class
---
+++ SessionDemo/WebForm1.aspx.vb
@@ -0,0 +1,17 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim name As String = Request.QueryString("UserName")
+        If name Is Nothing OrElse name.Length = 0 Then
+            Response.Redirect("Login.aspx")
+        Else
+            Label1.Text = "Welcome " & name
+        End If
+    End Sub
+    Protected Sub LogoutButtonClick(sender As Object, e As EventArgs)
+        Session.Abandon()
+        Response.Redirect("Login.aspx")
+    End Sub
+
+End Class
