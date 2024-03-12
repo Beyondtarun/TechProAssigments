963f92fdc569e99bacdd12731393496a37f31b82
Beyondtarun <teg.tarun@gmail.com>
2024-03-12 15:18:01 +0530

    day2

--- Training/Day1/script.js
+++ Training/Day1/script.js
@@ -1,10 +1,18 @@
-let a = document.getElementById("val1")
-let b = document.getElementById("val2")
-let p = document.getElementById("result")
+// let a = document.getElementById("val1")
+// let b = document.getElementById("val2")
+// let p = document.getElementById("result")
 let btn = document.getElementById("btn")
 
-btn.onclick= ()=>{
-    let num1 = parseFloat(a.value)
-    let num2 = parseFloat(b.value)
-    p.value = `sum is ${num1+num2}`;
-};
+// btn.onclick= ()=>{
+//     let num1 = parseFloat(a.value)
+//     let num2 = parseFloat(b.value)
+//     p.value = `sum is ${num1+num2}`;
+// };
+
+let color = document.getElementById("color");
+let box = document.getElementById("box")
+
+box.onclick=()=>{
+box.style.backgroundColor = color.value;
+}
+
